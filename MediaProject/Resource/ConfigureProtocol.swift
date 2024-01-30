ac91249d2c18760984b76e072817e4c4f1a590d3
LeeJaeheee <sns3434@gmail.com>
2024-01-30 17:07:16 +0900

    [Chore] 프로젝트 기본 설정

---
+++ MediaProject/Resource/ConfigureProtocol.swift
@@ -0,0 +1,28 @@
+//
+//  ConfigureProtocol.swift
+//  MediaProject
+//
+//  Created by 이재희 on 1/30/24.
+//
+
+import UIKit
+
+@objc protocol ConfigureProtocol {
+    func configureHierarchy()
+    func configureView()
+    func configureLayout()
+}
+
+@objc protocol VCProtocol: ConfigureProtocol {
+    @objc optional func configureNavigationItem()
+    @objc optional func setupActions()
+}
+
+protocol TableViewProtocol {
+    func configureTableView()
+}
+
+protocol CollectionViewProtocol {
+    func configureCollectionView()
+    func configureCollectionViewLayout() -> UICollectionViewFlowLayout
+}
---
+++ MediaProject/TV/TVViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TVViewController.swift
+//  MediaProject
+//
+//  Created by 이재희 on 1/30/24.
+//
+
+import UIKit
+
+class TVViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
