82df2f6ed4959e9fd36a1f523bbfdb1c90c357a9
LeeJaeheee <sns3434@gmail.com>
2024-02-03 21:11:48 +0900

    [Feat] 화면전환 함수 추가

--- MediaProject/Resource/BaseViewController.swift
+++ MediaProject/Resource/BaseViewController.swift
@@ -7,6 +7,13 @@
 
 import UIKit
 
+enum TransitionStyle {
+    case present
+    case presentNavigation
+    case presentFullNavigation
+    case push
+}
+
 class BaseViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -21,5 +28,23 @@ class BaseViewController: UIViewController {
     func configureHierarchy() { }
     func configureLayout() { }
     func configureView() { }
+    
+    func transition<T: UIViewController>(style: TransitionStyle, viewController: T.Type) {
+        let vc = T()
+        
+        switch style {
+        case .present:
+            present(vc, animated: true)
+        case .presentNavigation:
+            let nav = UINavigationController(rootViewController: vc)
+            present(nav, animated: true)
+        case .presentFullNavigation:
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: true)
+        case .push:
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 
 }
